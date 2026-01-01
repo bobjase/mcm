1cdb224bbc293266b2afafb0ee80f7454f65e972
bobjase <jase-github@slashbin.org>
2026-01-01 09:25:48 -0500

    -flto

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Compiling...
-C:\msys64\ucrt64\bin\g++ -pthread -DNDEBUG -O3 -fomit-frame-pointer -std=c++14 -Wno-deprecated-declarations -o mcm Archive.cpp Huffman.cpp MCM.cpp Memory.cpp Util.cpp Compressor.cpp File.cpp LZ.cpp Tests.cpp
+C:\msys64\ucrt64\bin\g++ -pthread -DNDEBUG -O3 -fomit-frame-pointer -march=x86-64 -mtune=generic -flto -std=c++14 -Wno-deprecated-declarations -o mcm Archive.cpp Huffman.cpp MCM.cpp Memory.cpp Util.cpp Compressor.cpp File.cpp LZ.cpp Tests.cpp
 if errorlevel 1 (
   echo Compilation failed.
 ) else (
